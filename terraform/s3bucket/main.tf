e18e58e9e40780e21a2bf6ad5cbca00cda0a8bf4
yuta-katayama-23 <yutapilafu@gmail.com>
2021-10-08 22:09:39 +0900

    variable名を変更

--- terraform/s3bucket/main.tf
+++ terraform/s3bucket/main.tf
@@ -21,7 +21,7 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-variable "user_name" {}
+variable "username" {}
 
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = "terraform-build-artifact-go-lambda"
@@ -49,7 +49,7 @@ data "aws_iam_policy_document" "policy_doc" {
 
     principals {
       type        = "AWS"
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${var.user_name}"]
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${var.username}"]
     }
 
     actions = [
